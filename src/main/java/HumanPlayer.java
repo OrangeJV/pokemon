3d5a2bf7dcd7ec964c8c184f121727e42758cfea
OrangeJV <147615681+OrangeJV@users.noreply.github.com>
2024-04-04 23:17:42 +0300

    Update HumanPlayer.java

--- src/main/java/HumanPlayer.java
+++ src/main/java/HumanPlayer.java
@@ -6,7 +6,7 @@
 
 import java.util.Scanner;
 
-public class HumanPlayer extends Player {
+public class HumanPlayer extends AbstractPlayer {
 
     /**
      * Constructor that instantiates a monster
